e356f4d30efbb5db273581435070b9e0a2f249a6
Greg K <gkreisman_cep@qq.com>
2023-03-29 10:26:13 +0800

    real problems

--- sketch5.js
+++ sketch5.js
@@ -1,5 +1,5 @@
-let zoom =10;
-let res = .25 // less than a whole  number  1, .5, .25, .125 plotting 600, 1200, 2400, and 4800 points 
+let zoom =20;
+let res = 1// less than a whole  number  1, .5, .25, .125 plotting 600, 1200, 2400, and 4800 points 
 // from - 300 to 300  (.125 looks like this (4800) [-300, -299.875, -299.75, -299.625, -299.5,])
 // but .5 looks like this (1200) [-300, -299.5, -299, -298.5, -298, 
 let cnv;
@@ -26,8 +26,10 @@ function setup() {
   plotfun(p1y, color(255, 255, 0));
   plotfun(p2y, color(0, 255, 255));
   print("hello",p1y)
+  print(p2y)
   //plotfun("sin(2*x)*80", color(255));
   //saveCanvas('myplot2', 'png');
+  cobweb(3)
 }
 /*
 function draw(){
@@ -77,4 +79,28 @@ function plotfun(yarr, clr) {
    ellipse(x*zoom,cy*zoom,5,5) // calculated points plotted on vertex
   }
   endShape();
+}
+
+
+function cobweb(strt){
+  print("hello cobweb")
+  let tx = strt
+  let ty = strt
+  let nexty, nextx
+  print(ty)
+  ellipse(tx*zoom,-ty*zoom, 30,30)
+  // get that positionin the array
+  for (let i =0; i<3; i++){
+     nextx = tx
+     nexty = p2y[tx+((600)/2)] 
+    line(tx*zoom,-ty*zoom, nextx*zoom,-nexty*zoom)
+      tx =floor(nextx)
+      ty = nexty
+      print(tx,ty)
+    //line(nextx*zoom,-nexty*zoom ,tx*zoom, -ty*zoom)
+    // get next xy point
+    
+  }
+  
+
 }
