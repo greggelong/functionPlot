e5eecdd6280c8a94e5e86b8b8432e87d4d400b88
Greg K <gkreisman_cep@qq.com>
2023-04-02 13:28:01 +0800

    changed the array to an array of functions

--- sketch8.js
+++ sketch8.js
@@ -11,12 +11,31 @@ let p2y = [];
 //let eq1=  "(-0.045*x)**3 - 3"//"-1*(0.2*x)**2 +45"
 //let eq1= "(.33*x)-3"
 //let eq1= "(-.33*x)-3"
-let eq1 = "x-0.3*x**2+30"
-let cobplot = -25
+let eq1 = "x-0.3*x**2+30";
+let cobplot = -25;
 
-let eqs= ["-1*(0.2*x)**2 +45","x-0.3*x**2+30","(0.045*x)**3 - 3", "(-0.045*x)**3 - 3", "(.33*x)-3", "(-.33*x)-3"]
-let eqcount= 0;
-let fc=-1
+let eqs = [
+  function (x) {
+    return -1 * (0.2 * x) ** 2 + 45;
+  },
+  function (x) {
+    return x - 0.3 * x ** 2 + 30;
+  },
+  function (x) {
+    return (0.045 * x) ** 3 - 3;
+  },
+  function (x) {
+    return (-0.045 * x) ** 3 - 3;
+  },
+  function (x) {
+    return 0.33 * x - 3;
+  },
+  function (x) {
+    return -0.33 * x - 3;
+  },
+];
+let eqcount = 0;
+let fc = -1;
 
 function setup() {
   cnv = createCanvas(600, 600);
@@ -29,47 +48,41 @@ function setup() {
   strokeWeight(1);
   translate(width / 2, height / 2); // move to center
   plotaxis();
-  
-  
 }
 
-function draw(){
-  
-  if (fc%111==0){
-    background(0)
-    
-    cobplot =57
-    eqcount++
-    eqcount=eqcount%eqs.length
+function draw() {
+  if (fc % 111 == 0) {
+    background(0);
+
+    cobplot = 57;
+    eqcount++;
+    eqcount = eqcount % eqs.length;
   }
   translate(width / 2, height / 2); // move to center
-  background(0,10);
+  background(0, 10);
   //print(eqcount)
   //print(zoom)
   plotaxis();
   plotfun(color(255));
-  cobweb2(cobplot)
-  cobplot ++
-  if (cobplot>56){
-    cobplot = -55
-    for (let i =-55; i<56;i++){
-      cobweb2(i)
+  cobweb2(cobplot);
+  cobplot++;
+  if (cobplot > 56) {
+    cobplot = -55;
+    for (let i = -55; i < 56; i++) {
+      cobweb2(i);
     }
     //saveCanvas('myCanvas', 'png');
   }
-  fc++
-  
- 
+  fc++;
 }
-
 
 function plotaxis() {
   //axis
   stroke(80);
   line(0, -height / 2, 0, height); //vert
   line(-width / 2, 0, width, 0); //horizontal
-  stroke(255)
-  line(-width/2,height/2,width/2,-height/2)
+  stroke(255);
+  line(-width / 2, height / 2, width / 2, -height / 2);
 }
 
 function getValues(arr, eq) {
@@ -88,15 +101,13 @@ function plotfun(clr) {
   beginShape();
   //let myeq = eval(eq)
   for (let x = -width / 2; x < width / 2; x += res) {
-    let fy = eval(eqs[eqcount]); //so it gets the values from the array
+    let fy = (eqs[eqcount](x)); //so it gets the values from the array
     let cy = fy * -1; // cy is corrected for plotting like cartesian not computer
     vertex(x * zoom, cy * zoom);
     //ellipse(x * zoom, cy * zoom, 5, 5); // calculated points plotted on vertex
   }
   endShape();
 }
-
- 
 
 function cobweb2(strt) {
   //print("hello cobweb2");
@@ -105,14 +116,14 @@ function cobweb2(strt) {
   let x = strt;
   let y = strt;
   let nexty, nextx;
-  strokeWeight(2)
+  strokeWeight(2);
   //print(y);
-  stroke(random(100,255),random(255), random(100,255));
+  stroke(random(100, 255), random(255), random(100, 255));
   //ellipse(tx * zoom, -ty * zoom, 30, 30);
   // get that positionin the array
   for (let i = 0; i < 10; i++) {
     nextx = x;
-    nexty = eval(eqs[eqcount]);  // getting the values by iteration
+    nexty = (eqs[eqcount](x)); // getting the values by iteration
     line(x * zoom, -y * zoom, nextx * zoom, -nexty * zoom);
     x = nexty;
     y = nexty;
